dab4e3e2a7fc5265c0275c7035e1c222f2a597e8
Hooooooon <shonn2323@gmail.com>
2021-07-31 17:06:04 +0900

    Feat: 추가 좋아요 및 저장한 포스트 보기

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.http.response import HttpResponseRedirect
 from django.http import HttpResponseForbidden
 from django.contrib import messages
@@ -100,3 +101,36 @@ class Photofavorite(View):
                 else :
                     photo.favorite.add(user)
             return HttpResponseRedirect('/')
+
+class PhotoLikeList(ListView):
+    model = Photo
+    template_name = 'photo/photo_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:  # 로그인확인
+            messages.warning(request, '로그인을 먼저하세요')
+            return HttpResponseRedirect('/')
+        return super(PhotoLikeList, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        # 내가 좋아요한 글을 보여주
+        user = self.request.user
+        queryset = user.like_post.all()
+        return queryset
+
+class PhotoFavoriteList(ListView):
+    model = Photo
+    template_name = 'photo/photo_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:   # 로그인 확인
+            messages.warning(request, '로그인을 먼저하세요')
+            return HttpResponseRedirect('/')
+        return super(PhotoFavoriteList, self).dispatch(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        # 내가 담은 글 보여주기
+        user = self.request.user
+        queryset = user.favorite_post.all()
+        return queryset
+
